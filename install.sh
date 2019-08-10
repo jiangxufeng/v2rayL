88cdf94cd74871ac8c46e74ab8582ce851f24149
jiangxufeng <jiangxufeng1998@qq.com>
2019-08-10 20:06:24 +0800

    简易版1.0

--- install.sh
+++ install.sh
@@ -12,8 +12,10 @@ echo "设置权限和快捷方式"
 project_path=$(cd `dirname $0`; pwd)
 current_user=$USER
 echo $current_user
-echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.zasshrc
-echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.bashrc   
+echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.zshrc
+echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.bashrc  
+source ~/.zshrc
+source ~/.bashrc 
 echo "$current_user ALL=NOPASSWD:/bin/systemctl restart v2rayL.service,/bin/systemctl start v2rayL.service,/bin/systemctl stop v2rayL.service,/bin/systemctl status v2rayL.service,/bin/systemctl enable v2rayL.service" | sudo tee -a /etc/sudoers
 echo "设置开机自启动"
 sudo systemctl enable v2rayL.service
