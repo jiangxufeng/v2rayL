faf8ac9165e5ddd3280094b8305d4299248c6ab4
jiangxufeng <jiangxufeng1998@qq.com>
2019-08-13 23:59:35 +0800

    更新install

--- install.sh
+++ install.sh
@@ -11,13 +11,22 @@ sudo cp ./v2ray-core/v2rayL.service /etc/systemd/system/
 echo "设置权限和快捷方式"
 sudo chmod 777 -R /etc/v2rayL
 sudo chmod 777 -R /usr/bin/v2rayL
-
+echo "复制静态文件"
+cp -r ./v2rayL-GUI/images /etc/v2rayL
+echo "移动可执行程序"
+cp ./v2rayL-exec/v2rayL /usr/bin/v2rayL/
 
 project_path=$(cd `dirname $0`; pwd)
 current_user=$USER
+
 echo $current_user
+
+echo "[Desktop Entry]\nType=Application\nExec=/usr/bin/v2rayL/v2rayL\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[zh_CN]=v2rayL
+Name=v2rayL\nComment[zh_CN]=V2rayL\nComment=v2ray-v2rayL" > /home/${current_user}/.config/autostart/v2rayL.desktop
+
 echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.zshrc
 echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.bashrc  
+
 source ~/.zshrc
 source ~/.bashrc 
 echo "$current_user ALL=NOPASSWD:/bin/systemctl restart v2rayL.service,/bin/systemctl start v2rayL.service,/bin/systemctl stop v2rayL.service,/bin/systemctl status v2rayL.service,/bin/systemctl enable v2rayL.service" | sudo tee -a /etc/sudoers
