8649dd96071861f9ccd5ec0378b704f3bd882c19
jiangxufeng <jiangxufeng1998@qq.com>
2019-08-14 00:43:03 +0800

    更新部分bug

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ V2Ray 用户手册：https://www.v2ray.com
 V2Ray 项目地址：https://github.com/v2ray/v2ray-core
 
 # v2rayL
-v2ray linux 客户端1.0。python3.6 + v2ray内核，系统版本ubuntu18.04
+v2ray linux 客户端1.1。python3.6 + v2ray内核，系统版本ubuntu18.04
 
-1.0版本个人使用，简易实现终端操作。目前支持vmess、shadowsocks
+目前支持vmess、shadowsocks
 
 - 添加订阅地址，自动解析并展示可连接VPN
 - 连接和断开VPN，连接完毕后终端可关闭
@@ -21,17 +21,5 @@ v2ray linux 客户端1.0。python3.6 + v2ray内核，系统版本ubuntu18.04
 ①       ./install.sh
 ```
 
-```
- ②       v2rayL
-```
-
-# 展示
-
-![主页](http://cloud.thinker.ink/images/e696559ba97bc730c996d20df938be09.png)
 
 
-![连接更换VPN](http://cloud.thinker.ink/images/a6a7b8de519e89559bcb9fdfb4b77afd.png)
-
-![配置操作](http://cloud.thinker.ink/images/9acbe8b0b02b109336e94d8d9df49b4f.png)
-
-![订阅操作](http://cloud.thinker.ink/images/1b73e5d63328234bcea5f9888ee9d083.png)
--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@ sudo chmod 777 -R /usr/bin/v2rayL
 echo "复制静态文件"
 cp -r ./v2rayL-GUI/images /etc/v2rayL
 echo "移动可执行程序"
-cp ./v2rayL-exec/v2rayL /usr/bin/v2rayL/
+cp ./v2rayL-exec/v2rayLui /usr/bin/v2rayL/
 
 project_path=$(cd `dirname $0`; pwd)
 current_user=$USER
 
 echo $current_user
 
-echo "[Desktop Entry]\nType=Application\nExec=/usr/bin/v2rayL/v2rayL\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[zh_CN]=v2rayL
+echo "[Desktop Entry]\nType=Application\nExec=/usr/bin/v2rayL/v2rayLui\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[zh_CN]=v2rayL
 Name=v2rayL\nComment[zh_CN]=V2rayL\nComment=v2ray-v2rayL" > /home/${current_user}/.config/autostart/v2rayL.desktop
 
 echo "alias v2rayL='python ${project_path}/v2rayL.py'" | sudo tee -a ~/.zshrc
@@ -32,4 +32,5 @@ source ~/.bashrc
 echo "$current_user ALL=NOPASSWD:/bin/systemctl restart v2rayL.service,/bin/systemctl start v2rayL.service,/bin/systemctl stop v2rayL.service,/bin/systemctl status v2rayL.service,/bin/systemctl enable v2rayL.service" | sudo tee -a /etc/sudoers
 echo "设置开机自启动"
 sudo systemctl enable v2rayL.service
+/usr/bin/v2rayL/v2rayLui &
 echo "完成."
